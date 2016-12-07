c110cbf5d270bc0546be6ffd8bcc92d859f3a206
Attawit Chaiyaroj <ahm.913@hotmail.com>
2016-12-07 21:58:53 +0700

    Finished Entity

--- src/model/Entity.java
+++ src/model/Entity.java
@@ -1,15 +1,87 @@
 package model;
 
-public abstract class Entity<D extends IDirection> implements IRenderable
+public abstract class Entity implements IRenderable
 {
-	protected int x, y, z;
-	protected int nextX, nextY;
-	protected D direction, nextDirection;
+	protected double x, y;
+	protected double nextX, nextY;
+	protected int z;
 	protected int speed;
-	protected boolean visible;
-	protected boolean isDestroyed, toBeDestroyed;
-	private int movingDelay, movingDelayCounter;
-	Field field;
+	protected boolean isDestroyed, isDestroyedInNextState;
+	protected Field field;
+	
+	public Entity(Field field, double x, double y, int speed)
+	{
+		this.field = field;
+		this.field.addEntity(this);
+		
+		this.x = this.nextX = x;
+		this.y = this.nextY = y;
+		this.speed = speed;
+		this.isDestroyed = this.isDestroyedInNextState = false;
+		
+		calculateNextState();
+	}
+
+	abstract public void update();
+	
+	abstract protected void calculateNextState();
+	
+	protected boolean move()
+	{
+		if(isDestroyed)
+			return false;
+		x = nextX;
+		y = nextY;
+		isDestroyed = isDestroyedInNextState;
+		calculateNextState();
+		return true;
+	}
+	
+	@Override
+	public boolean isVisible()
+	{
+		return !isDestroyed;
+	}
+	
+	public boolean isSamePosition(Entity other)
+	{
+		return this.x == other.x && this.y == other.y;
+	}
+
+	public boolean isDestroyed()
+	{
+		return isDestroyed;
+	}
+
+	public void setDestroyed(boolean isDestroyed)
+	{
+		this.isDestroyed = isDestroyed;
+	}
+
+	public double getX()
+	{
+		return x;
+	}
+
+	public double getY()
+	{
+		return y;
+	}
+
+	public double getNextX()
+	{
+		return nextX;
+	}
+
+	public double getNextY()
+	{
+		return nextY;
+	}
+
+	public int getSpeed()
+	{
+		return speed;
+	}
 	
 	
 }
