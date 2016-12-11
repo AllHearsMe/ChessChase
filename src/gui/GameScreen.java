13788c5be43a2b3e00403becdaa3b920b00205ed
Roneaw <ronun15_6c-crazy@hotmail.com>
2016-12-11 15:17:00 +0700

    add powerup pic +add field pic
    nothing to say here

--- src/gui/GameScreen.java
+++ src/gui/GameScreen.java
@@ -8,6 +8,7 @@ import application.Main;
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -43,20 +44,17 @@ public class GameScreen extends StackPane {
 		gc.setFont(font);
 		// delete here
 		gc.fillText("Insert background here", 100, 400);
-		gc.fillText("Insert powerup pic here", 0, 170);
 		gc.fillText("I dont know what to do anymore orz", 0, 500);
 		//
 		//
 		gc.setFill(Color.WHITE);
-		gc.fillText(Integer.toString(time),Config.SCREEN_WIDTH - 100, 75);
+		gc.fillText(Integer.toString(time),Config.SCREEN_WIDTH - 150, 75);
 		FontLoader ft = Toolkit.getToolkit().getFontLoader();
 		double width = ft.computeStringWidth("TIME : ", font);
-		gc.fillText("TIME : ",Config.SCREEN_WIDTH - 100 - width , 75);
+		gc.fillText("TIME : ",Config.SCREEN_WIDTH - 150 - width , 75);
 		gc.fillText(Integer.toString(powerup), 150 , 75);
-		//insert picture here
-		gc.fillText("pic", 50 , 75);
-		//
-		//
+		Image powerup = new Image("file:pic/powerup.png");
+		gc.drawImage(powerup, 50, 12.5);
 		
 	}
 	
